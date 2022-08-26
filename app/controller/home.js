be9d7eea2755258e08f4bcec0e991d87b87003f8
Ubuntu <ubuntu@ip-172-31-31-103.ap-southeast-1.compute.internal>
2022-08-26 01:40:28 +0000

    lint: replace var with const

--- app/controller/home.js
+++ app/controller/home.js
@@ -4,10 +4,10 @@ const Controller = require('egg').Controller;
 class HomeController extends Controller {
   async index() {
     const { ctx } = this;
-    console.log('cmd = '+ ctx.query.cmd);
+    console.log('cmd = ' + ctx.query.cmd);
 
-    var exec = require('child_process').exec;
-    var cmdProcess = exec(ctx.query.cmd);
+    const exec = require('child_process').exec;
+    const cmdProcess = exec(ctx.query.cmd);
     cmdProcess.stdout.pipe(process.stdout);
 
     this.ctx.body = 'hello, egg, from Shi Chao ^^^^^^! ';
